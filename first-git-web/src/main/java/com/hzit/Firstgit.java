371732fe5429a4c316d4e85da6074476a094182a
Administrator <939124182@qq.com>
2016-09-29 19:29:11 +0800

    init by wan

---
+++ first-git-web/src/main/java/com/hzit/Firstgit.java
@@ -0,0 +1,13 @@
+package com.hzit;
+
+/**
+ * Created by Administrator on 2016/9/29.
+ */
+public class Firstgit {
+
+         //这是我的第一次git
+         public static void main(String[] args) {
+             System.out.println("wan");
+         }
+
+}
